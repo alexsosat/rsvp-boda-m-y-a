9c0d02ff71adef94d8cab38a37cacfe733a6d5e1
alexsosat <alexsosat@gmail.com>
2026-02-27 01:33:00 -0600

    Adds content builder classes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,20 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "script-src": ["https://cdnjs.cloudflare.com"],
+          "media-src": ["https://cdnjs.cloudflare.com"],
+          "img-src": ["https://cdnjs.cloudflare.com"],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
